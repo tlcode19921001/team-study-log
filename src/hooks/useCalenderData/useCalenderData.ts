a2ac368e782bef86e33c06e3e92f96eb016e8a1d
neatcode-web <thomasfeng101@gmail.com>
2022-06-02 17:09:34 -0400

    feat: Add sample structure

--- src/hooks/useCalenderData/useCalenderData.ts
+++ src/hooks/useCalenderData/useCalenderData.ts
@@ -1,23 +1,5 @@
-import { useCallback, useState } from 'react';
-import { mockData } from '../../api/attendance';
-import type { Attendance } from '../../api/attendance';
+import { Attendence } from './useCalenderData.helper';
 
-/**
- * @description
- * hook that handles attendance data for users
- * covers basic CRUD for data.
- */
-function useCalenderData() {
-  const [attendence, setAttendence] = useState(mockData);
-
-  // Load attendance data
-
-  // Update attendance data
-  const addAttendence = useCallback((newData: Attendance) => {
-    setAttendence((prev) => [...prev, newData]);
-  }, []);
-
-  return { attendence, addAttendence };
-}
+function useCalenderData() {}
 
 export default useCalenderData;
