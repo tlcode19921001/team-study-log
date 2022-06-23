f81fbca8c09349867e22ebbc156e0e8551c63262
neatcode-web <thomasfeng101@gmail.com>
2022-06-23 04:42:52 -0400

    design: Defined shadow design system and refactor styles

--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signin } from '../api/auth';
 import AuthFormTemplate from '../components/AuthFormTemplate';
+import AuthFormLayout from '../components/layouts/AuthFormLayout';
 import { LoadingButton } from '../components/shared/Button';
 import Input from '../components/shared/Input';
 import useForm from '../hooks/shared/useForm';
@@ -36,42 +37,44 @@ const SigninPage = () => {
   });
 
   return (
-    <AuthFormTemplate
-      title="Login"
-      controls={
-        <>
-          <Input
-            disabled={loading}
-            name="username"
-            placeholder="username"
-            value={data.username}
+    <AuthFormLayout>
+      <AuthFormTemplate
+        title="Login"
+        controls={
+          <>
+            <Input
+              disabled={loading}
+              name="username"
+              placeholder="username"
+              value={data.username}
+              variant="primary"
+              onChange={handleChange}
+            />
+            <Input
+              disabled={loading}
+              name="password"
+              placeholder="password"
+              type="password"
+              value={data.password}
+              variant="primary"
+              onChange={handleChange}
+            />
+          </>
+        }
+        trigger={
+          <LoadingButton
+            fullWidth
+            isLoading={loading}
+            size="lg"
+            type="submit"
             variant="primary"
-            onChange={handleChange}
-          />
-          <Input
-            disabled={loading}
-            name="password"
-            placeholder="password"
-            type="password"
-            value={data.password}
-            variant="primary"
-            onChange={handleChange}
-          />
-        </>
-      }
-      trigger={
-        <LoadingButton
-          fullWidth
-          isLoading={loading}
-          size="lg"
-          type="submit"
-          variant="primary"
-        >
-          Signin
-        </LoadingButton>
-      }
-      onSubmit={handleSubmit}
-    />
+          >
+            Signin
+          </LoadingButton>
+        }
+        onSubmit={handleSubmit}
+      />
+    </AuthFormLayout>
   );
 };
 
