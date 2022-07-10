c207169cbada1367f0aedb37b4f28c85dc644b50
neatcode-web <thomasfeng101@gmail.com>
2022-07-10 18:19:18 -0400

    chore: Update test code

--- src/__tests__/dateFormat.test.ts
+++ src/__tests__/dateFormat.test.ts
@@ -1,30 +1,31 @@
-import { formatDate, formatTime } from '../utils/dateFormat';
+import { formatDate, formatUTCTime } from '../utils/dateFormat';
 
 describe('date format', () => {
   it('format date, options: { format: "YYYY-MM-DD", delimiter: "-" }', () => {
     expect(
       formatDate(new Date(2022, 9, 9), { format: 'YYYY-MM-DD', delimiter: '-' })
-    ).toBe('2022-06-09');
+    ).toBe('2022-10-09');
   });
 
   it('format date, options: { format: "YYYY-MM", delimiter: "%" }', () => {
     expect(
       formatDate(new Date(2022, 9, 9), { format: 'YYYY-MM', delimiter: '%' })
-    ).toBe('2022%06');
+    ).toBe('2022%10');
   });
 
   it('format date, options: { format: "MM-DD", delimiter: " " }', () => {
     expect(
       formatDate(new Date(2022, 9, 9), { format: 'MM-DD', delimiter: ' ' })
-    ).toBe('06 09');
+    ).toBe('10 09');
   });
 
+  // Error is occured when use getHours, getMinutes for the timezone different with CI env
   it('format time, options: { format: "HH:MM:SS", delimiter: ":" }', () => {
     expect(
-      formatTime(new Date(1667975238660), {
+      formatUTCTime(new Date(1667975238660), {
         format: 'HH:MM:SS',
         delimiter: ':',
       })
-    ).toBe('15:27:18');
+    ).toBe('06:27:18');
   });
 });
