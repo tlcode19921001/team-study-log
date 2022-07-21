05514e63baddc5e4fc1bbf62bab5f923774917ae
neatcode-web <thomasfeng101@gmail.com>
2022-07-21 12:17:35 -0400

    feat: add api to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,60 @@
+import React, { useState } from 'react';
+import { getOrganizationList } from '../../api/organization';
+import { Organization } from '../../api/types';
 import Dropdown from '../shared/Dropdown';
 import Input from '../shared/Input';
 import NavigationBar from '../shared/NavigationBar';
+import formatOrganization from './Header.helper';
 
-// TODO - Add Dropdown to Header
+type Item = { key: string | number; value: React.ReactNode };
+
 const Header = () => {
-  const items = [
-    { key: '1', value: 'value1' },
-    { key: '2', value: 'value2' },
-  ];
+  const [inputValue, setInputValue] = useState<string>('');
+  const [organizations, setOrganizations] = useState<Organization[]>([]);
+
+  const items = organizations.map<Item>((organization) => ({
+    key: organization.id,
+    value: organization.name,
+  }));
+
+  const getOrganizations = async (query: string) => {
+    if (!query) return;
+
+    try {
+      const data = await getOrganizationList(query);
+      setOrganizations(formatOrganization(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+
+    const value = e.target.value.trim();
+
+    if (!value) {
+      setOrganizations([]);
+      return;
+    }
+
+    await getOrganizations(value);
+  };
+
   return (
     <NavigationBar
       left={<div>Team Study Log</div>}
       right={
-        <Dropdown items={items} trigger={<Input placeholder="Search By Group" />} />
+        <Dropdown
+          items={items}
+          trigger={
+            <Input
+              placeholder="Search in Group"
+              value={inputValue}
+              onChange={handleChange}
+            />
+          }
+        />
       }
     />
   );
